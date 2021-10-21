909cd91d7779560a8836d65c08aecb6168c92225
PlotnikovZhenia <niaplotzhekovmi@gmail.com>
2021-10-22 00:00:09 +0300

    homework #2

--- script.js
+++ script.js
@@ -24,3 +24,35 @@ navbar.onclick = function(event) {
       }
       a.classList.add('navbar-active');
     }
+    function OpenWindowTask() {
+      let messeage=document.createElement('div');
+      messeage.className="WindowTasksOpen";
+      messeage.innerHTML="<h2>Are you sure you want to change the number of tasks ?</h2><div><p onclick='CompletedWindowTask()'>Yes</p><p onclick='CloseWindowTask()'>No</p></div>";
+      document.body.append(messeage);
+    }
+    function CompletedWindowTask() {
+      let elemOfComplete=document.getElementById('resultCompletedTasks');
+      let elemOfOpenTasks=document.getElementById('resultOpenTasks');
+      let messeage=document.querySelector('.WindowTasksOpen')
+      messeage.className="";
+      if(elemOfOpenTasks.innerHTML>0){
+        elemOfOpenTasks.innerHTML=elemOfOpenTasks.innerHTML-1;
+        elemOfComplete.innerHTML++;
+      };
+    }
+
+    function CloseWindowTask() {
+      let messeage=document.querySelector('.WindowTasksOpen')
+      messeage.className="";
+    }
+    //array of images-notification
+    function indexOfImage(src) {
+      let array_images=['Symbol12.jpg', 'Symbol13.jpg','Symbol14.jpg','Symbol15.jpg'];
+      for(let i=0; i<array_images.length;i++) {
+        if(src.endsWith(array_images[i])){
+          notificationIndex.innerHTML=i+1;
+        }
+      }
+    }
+
+
